d884eb66b77ffcc8220cbad93adb23cacbe87267
typicode <typicode@gmail.com>
2017-01-05 01:42:18 +0100

    add Ramda test

--- test/fp.js
+++ test/fp.js
@@ -2,22 +2,37 @@ const fs = require('fs')
 const path = require('path')
 const test = require('tape')
 const tempfile = require('tempfile')
-const defaults = require('lodash/fp/defaults')
-const get = require('lodash/fp/get')
-const concat = require('lodash/fp/concat')
-const set = require('lodash/fp/set')
+const fp = require('lodash/fp')
+const R = require('ramda')
 const low = require('../src/fp.node')
 
-test('fp', (t) => {
+test('fp + lodash/fp', (t) => {
   const filename = tempfile()
   const db = low(filename)
 
   const posts = db('posts', [])
 
-  t.same(posts(concat(1)), [ 1 ], 'should return a new array')
+  t.same(posts(fp.concat(1)), [ 1 ], 'should return a new array')
   t.same(db.getState(), {}, 'shouldn\'t change state')
 
-  posts.write(concat(1))
+  posts.write(fp.concat(1))
+  t.same(db.getState(), { posts: [ 1 ] }, 'should change state')
+
+  const actual = JSON.parse(fs.readFileSync(filename))
+  t.same(actual, { posts: [ 1 ] })
+  t.end()
+})
+
+test('fp + ramda', (t) => {
+  const filename = tempfile()
+  const db = low(filename)
+
+  const posts = db('posts', [])
+
+  t.same(posts(R.concat([ 1 ])), [ 1 ], 'should return a new array')
+  t.same(db.getState(), {}, 'shouldn\'t change state')
+
+  posts.write(R.concat([ 1 ]))
   t.same(db.getState(), { posts: [ 1 ] }, 'should change state')
 
   const actual = JSON.parse(fs.readFileSync(filename))
