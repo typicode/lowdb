76886aa636cafdad21d02a29485a4771d2cd3a6a
Noah Swanson <swan@0101.solar>
2018-05-18 18:25:20 -0400

    Fixed the formatting to follow the original

--- src/adapters/DatArchive.js
+++ src/adapters/DatArchive.js
@@ -1,26 +1,31 @@
-/* global DatArchive */
 /* global window */
 
 const Base = require('./Base')
 
 class DatArchive extends Base {
   read() {
-  	let archive = new DatArchive(window.location.toString())
-  	archive.stat(this.source).then(_ => {
-	  	let data = archive.readFile(this.source).then(data => {
-				const trimmed = data.trim()
-				return trimmed ? this.deserialize(trimmed) : this.defaultValue
-	  	})
-			.catch(e => {
-			  if (e instanceof SyntaxError) {
-			    e.message = `Malformed JSON in file: ${this.source}\n${e.message}`
-			  }
-			  throw e
-			})  		
-  	}).catch(e => {
-  		return archive.writeFile(this.source, this.serialize(this.defaultValue)).then(() => this.defaultValue)
-  	})
-
+    let archive = new DatArchive(window.location.toString())
+    archive
+      .stat(this.source)
+      .then(_ => {
+        archive
+          .readFile(this.source)
+          .then(data => {
+            const trimmed = data.trim()
+            return trimmed ? this.deserialize(trimmed) : this.defaultValue
+          })
+          .catch(e => {
+            if (e instanceof SyntaxError) {
+              e.message = `Malformed JSON in file: ${this.source}\n${e.message}`
+            }
+            throw e
+          })
+      })
+      .catch(e => {
+        return archive
+          .writeFile(this.source, this.serialize(this.defaultValue))
+          .then(() => this.defaultValue)
+      })
   }
 
   write(data) {
