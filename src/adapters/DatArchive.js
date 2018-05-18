46466da047fb6515cbc19c3d4384c184d970c342
Noah Swanson <swan@0101.solar>
2018-05-18 17:50:48 -0400

    Added DatArchive adapter. (see https://www.beakerbrowser.com/docs/apis/dat.html)

---
+++ src/adapters/DatArchive.js
@@ -0,0 +1,32 @@
+/* global DatArchive */
+/* global window */
+
+const Base = require('./Base')
+
+class DatArchive extends Base {
+  read() {
+  	let archive = new DatArchive(window.location.toString())
+  	archive.stat(this.source).then(data => {
+	  	let data = archive.readFile(this.source).then(data => {
+				const trimmed = data.trim()
+				return trimmed ? this.deserialize(trimmed) : this.defaultValue
+	  	})
+			.catch(e => {
+			  if (e instanceof SyntaxError) {
+			    e.message = `Malformed JSON in file: ${this.source}\n${e.message}`
+			  }
+			  throw e
+			})  		
+  	}).catch(e => {
+  		return archive.writeFile(this.source, this.serialize(this.defaultValue)).then(() => this.defaultValue)
+  	})
+
+  }
+
+  write(data) {
+    let archive = new DatArchive(window.location.toString())
+    return archive.writeFile(this.source, this.serialize(this.defaultValue)).then(() => true)
+  }
+}
+
+module.exports = DatArchive
