7166e2e278779e7b40bb51cb596f704201b565d3
typicode <typicode@gmail.com>
2023-10-23 15:48:28 +0200

    fix: node presets with url

--- src/presets/node.ts
+++ src/presets/node.ts
@@ -1,30 +1,30 @@
+import fs from 'node:fs'
+
 import { Memory, MemorySync } from '../adapters/Memory.js'
 import { JSONFile, JSONFileSync } from '../adapters/node/JSONFile.js'
 import { Low, LowSync } from '../core/Low.js'
 
 export async function JSONPreset<Data>(
-  filename: string | URL,
+  filename: fs.PathLike,
   defaultData: Data,
-  dir?: string | URL,
 ): Promise<Low<Data>> {
   const adapter =
     process.env.NODE_ENV === 'test'
       ? new Memory<Data>()
-      : new JSONFile<Data>(new URL(filename, dir))
+      : new JSONFile<Data>(filename)
   const db = new Low<Data>(adapter, defaultData)
   await db.read()
   return db
 }
 
 export function JSONSyncPreset<Data>(
-  filename: string | URL,
+  filename: fs.PathLike,
   defaultData: Data,
-  dir?: string | URL,
 ): LowSync<Data> {
   const adapter =
     process.env.NODE_ENV === 'test'
       ? new MemorySync<Data>()
-      : new JSONFileSync<Data>(new URL(filename, dir))
+      : new JSONFileSync<Data>(filename)
   const db = new LowSync<Data>(adapter, defaultData)
   db.read()
   return db
